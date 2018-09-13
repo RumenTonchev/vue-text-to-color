1191723f0bf8679356f023bae6f184622c1191d1
RumenTonchev <Dogy9006@gmail.com>
2018-09-13 18:05:58 +0300

    Initial Commit

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,24 @@
 <template>
     <div>
+        <div class="text-to-speech" style="height: 0; overflow: hidden; padding: 0">
+            <vue-speech @onTranscriptionEnd="onEnd" lang="bg"/>
+        </div>
+        <div class="output-div">
+            {{inputField2}}
+        </div>
+        <div class="output-div">
+            <!--<template v-for="letter of inputField2" v-if="letterConvert(letter) !== 'not valid'">-->
+            <template v-for="letter of inputField2">
+                <div class="output-letter" :style="{background: letterConvert(letter)}"></div>
+            </template>
+        </div>
+        <hr>
         <div class="input-div">
             <input type="text" v-model="inputField">
         </div>
         <div class="output-div">
             <template v-for="letter of inputField" v-if="letterConvert(letter) !== 'not valid'">
-                <div class="output-letter" :style="{background: letterConvert(letter)}">
-                </div>
+                <div class="output-letter" :style="{background: letterConvert(letter)}"></div>
             </template>
         </div>
     </div>
@@ -18,7 +30,9 @@
         data() {
             return {
                 inputField: '',
-                outputArray: []
+                inputField2: '',
+                outputArray: [],
+                speechText: ''
             }
         },
         methods: {
@@ -26,22 +40,33 @@
                 let number = letter.toLowerCase().charCodeAt(0) - 97;
                 if (number === -65) {
                     return 'transparent';
-                } else if(number >=0 && number<= 25) {
-                    let disperseNumber = Math.floor(number * 4095 / 26);
+                } else if (number >= 0 && number <= 25) {
+                    let disperseNumber = Math.floor((number * 4095) / 26);
                     let resultString = disperseNumber.toString(16).padStart(3, '0');
+                    return `#${resultString}`;
+                } else if (number >= 975 && number <= 1006){
+                    number = number - 975;
+                    let disperseNumber = Math.floor((number * 4095) / 30);
+                    let resultString = disperseNumber.toString(16).padStart(3, '0');
+                    console.log(resultString);
                     return `#${resultString}`;
                 } else {
                     return 'not valid';
                 }
             },
+            onEnd({lastSentence, transcription}) {
+                this.inputField2 = lastSentence;
+                // `lastSentence` is the last sentence before the pause
+                // `transcription` is the full array of sentences
+            }
         }
     }
 </script>
 
-<style scoped>
-    .input-div, .output-div {
+<style>
+    .text-to-speech, .input-div, .output-div {
         width: 100%;
-        padding: 30px;
+        padding: 10px;
     }
 
     .output-letter {
@@ -49,6 +74,11 @@
         width: 20px;
         height: 20px;
         border: 1px solid transparent;
+    }
+
+    .text-to-speech p {
+        font-size: 10px;
+        margin: 1px;
     }
 
 
--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueSpeech from 'vue-speech'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(VueSpeech);
 
 new Vue({
   router,
